e39e491f86ad87a6f731af08be6dc35ada4342fe
Aashray Boddu <Aashray.Boddu@amd.com>
2025-10-24 00:10:13 -0500

    rde: re-format for clang-19
    re-format code for clang-19.
    
    Signed-off-by: Aashray Boddu <Aashray.Boddu@amd.com>

--- redfish-core/lib/rde.hpp
+++ redfish-core/lib/rde.hpp
@@ -1240,12 +1240,13 @@ inline void requestRoutesRDEService(crow::App& app)
                    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
                    const std::string& systemId, const std::string& deviceId,
                    const std::string& vendorPath) {
-        std::string baseUri = "/redfish/v1/Systems/" + systemId +
-                              "/NetworkAdapters/" + deviceId;
+                std::string baseUri = "/redfish/v1/Systems/" + systemId +
+                                      "/NetworkAdapters/" + deviceId;
 
-        handleRDEServicePath(app, req, asyncResp, systemId, deviceId,
-                             vendorPath, redfish::networkSchemaName, baseUri);
-    });
+                handleRDEServicePath(app, req, asyncResp, systemId, deviceId,
+                                     vendorPath, redfish::networkSchemaName,
+                                     baseUri);
+            });
 
     BMCWEB_ROUTE(app, "/redfish/v1/Systems/<str>/NetworkAdapters/<str>")
         .privileges(redfish::privileges::getProcessor)
@@ -1253,11 +1254,11 @@ inline void requestRoutesRDEService(crow::App& app)
             [&app](const crow::Request& req,
                    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
                    const std::string& systemId, const std::string& deviceId) {
-        std::string baseUri = "/redfish/v1/Systems/" + systemId +
-                              "/NetworkAdapters/" + deviceId;
-        handleRDEServiceRoot(app, req, asyncResp, systemId, deviceId,
-                             redfish::networkSchemaName, baseUri);
-    });
+                std::string baseUri = "/redfish/v1/Systems/" + systemId +
+                                      "/NetworkAdapters/" + deviceId;
+                handleRDEServiceRoot(app, req, asyncResp, systemId, deviceId,
+                                     redfish::networkSchemaName, baseUri);
+            });
 }
 
 } // namespace redfish
